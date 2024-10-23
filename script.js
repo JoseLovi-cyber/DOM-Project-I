f98195bb3c558f1741d6707676fe283e152d117b
Jose.76 <sorenjoselorenz@gmail.com>
2024-10-23 18:19:49 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const myDiv = document.getElementById("myDiv");
+myDiv.style.backgroundColor = "yellow";
+myDiv.style.margin = "20px";
+myDiv.style.padding = "10px";
+myDiv.style.fontSize = "18px";
+myDiv.style.fontWeight = "bold";
+myDiv.style.height = "200px";
+myDiv.style.width = "300px";
+myDiv.style.color = "blue";
